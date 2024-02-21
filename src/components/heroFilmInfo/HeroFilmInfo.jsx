37495ea01dde8d7731fc201e95348c1e072f56ff
jayanta tech <jayantapaikpara@gmail.com>
2024-02-21 13:57:02 +0530

    searchPage

--- src/components/heroFilmInfo/HeroFilmInfo.jsx
+++ src/components/heroFilmInfo/HeroFilmInfo.jsx
@@ -4,9 +4,15 @@ import ResponsiveText from "../responsiveText/ResponsiveText";
 import BothButton from "../bothButton/BothButton";
 import RattingBox from "../rattingBox/RattingBox";
 import DateBox from "../dateBox/DateBox";
-// import ResponsiveTitle from "../responsiveTitle/ResponsiveTitle";
-import ResponsiveTitle from "../responsiveTitle/ResponsiveTitle";
 const HeroFilmInfo = ({ data, imageAdd }) => {
+  function getTheTittle(titleWord) {
+    let newTitle = titleWord?.split("");
+    if (newTitle.length > 0 && newTitle?.length < 30) {
+      return newTitle.join("");
+    } else {
+      return newTitle?.slice(0, 28).join("") + "...";
+    }
+  }
   return (
     <div className=" w-full md:h-[280px] max-md:hidden absolute bottom-0 flex items-center justify-center my-8">
       <div className=" h-full lg:w-[70%] min-md:w-[70%] max-lg:w-[90%] max-md:translate-y-24 max-lg:-translate-y-48 bg-[#747272] backdrop-filter backdrop-blur-sm bg-opacity-60 md:px-4 z-30 grid grid-flow-col max-md:grid-flow-row md:grid-cols-5 max-md:grid-rows-4 gap-10 max-lg:gap-24 rounded-lg">
@@ -17,7 +23,10 @@ const HeroFilmInfo = ({ data, imageAdd }) => {
           />
         </div>
         <div className=" md:col-span-4 py-4 md:px-3 md:ml-6 max-md:px-3">
-          <ResponsiveTitle data={data} />
+          <h2 className=" text-3xl font-bold font-[Roboto] text-white">
+            {getTheTittle(data?.original_title)}
+          </h2>
+          {/* <ResponsiveTitle data={data} /> */}
           <div className=" w-full h-[100px] max-md:hidden">
             <ResponsiveText data={data} />
           </div>

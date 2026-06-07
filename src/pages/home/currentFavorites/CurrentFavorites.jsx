109e6566ca1f0650b1056e8d4a6f948875d2adee
Jayanta Biswas <jayanta.biswas@delostylestudio.com>
2026-06-07 15:40:01 +0530

    New changes

--- src/pages/home/currentFavorites/CurrentFavorites.jsx
+++ src/pages/home/currentFavorites/CurrentFavorites.jsx
@@ -30,7 +30,7 @@ const CurrentFavorites = () => {
       <ContentWrapper>
         <div className=" ">
           <h2 className="text-3xl font-bold font-[Roboto] pb-6">
-            Current Favorites
+            Current Favorites by jay
           </h2>
           <div className="border-4 border-[#30B170] px-2 rounded-lg shadow-xl pb-3">
             <FavoritesSlider data={data} loading={loading} />

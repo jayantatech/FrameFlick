b8293f3e27dab4b67fecc88710394e0c146c636f
jayanta biswas <jayanta.biswas@delostylestudio.com>
2026-07-29 23:50:47 +0530

    New changes for jenkins nodes

--- src/pages/home/currentFavorites/CurrentFavorites.jsx
+++ src/pages/home/currentFavorites/CurrentFavorites.jsx
@@ -30,7 +30,7 @@ const CurrentFavorites = () => {
       <ContentWrapper>
         <div className=" ">
           <h2 className="text-3xl font-bold font-[Roboto] pb-6">
-            Current Favorites for prod test
+            Current Favorites for jenkins test by jay
           </h2>
           
           <div className="border-4 border-[#30B170] px-2 rounded-lg shadow-xl pb-3">
